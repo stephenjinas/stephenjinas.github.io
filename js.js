49c41fbefa0f2d8d5dff01433ba1921472dfa7c8
Sunday <139649541+stephenjinas@users.noreply.github.com>
2024-01-17 01:09:16 -0800

    Add files via upload
    my first html file

---
+++ js.js
@@ -0,0 +1,6 @@
+function myfunction(){
+   document.getElementById("pas").innerHTML="the password you enter is incorrect";
+}
+function myfunction(){
+ document.getElementById("sunny").innerHTML="hi  im coming";
+}
